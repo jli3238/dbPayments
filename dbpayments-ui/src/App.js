264e0865ebe491726f62f186d51ad4063311e8fa
Jiahong Liu <jiahong.liu@dbtechhackathon.com>
2021-02-25 17:37:59 -0600

    Add navbar buttons, background image, and toggles

---
+++ dbpayments-ui/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        {/* {/* <header className="App-header"> */}
+            {/* Welcome to Debba */}
+        {/* </header> */}
+        <div className='title'>Debaa</div>
+        <div className='login'>
+          <input type='email' placeholder='Email'></input>
+          <input type='password' placeholder='Password'></input>
+        </div>
+        <div>
+          <img src='public/logo192' alt='192'></img>
+          <img src='logo512' alt='512'></img>
+        </div>
+        <div>
+        <button>CONTINUE</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/Main.js
+++ src/Main.js
@@ -4,16 +4,45 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
+
+const iconPath = process.env.PUBLIC_URL + '/assets/icons/';
  
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.openNavbar = this.openNavbar.bind(this);
+    this.openDebba = this.openDebba.bind(this);
+    this.state = { isOpen: false, isNavbarOpen: false };
+  }
+
+  openNavbar() {
+    this.setState({ isNavbarOpen: !this.state.isNavbarOpen});
+    this.setState({ isOpen: false })
+  }
+
+  openDebba() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+
   render() {
     return (
       <HashRouter>
-        <div id="mobileBox">
+        <div className="container">
+          <div className="avatar"><div onClick={this.openNavbar}><img src={`${iconPath}faceid.jpg`} alt=""/></div></div> */}
+          {this.state.isNavbarOpen && 
+            <div className="navbar">
+              <div><img src={`${iconPath}screenreader.jpg`} alt=""/></div>
+              <div><img src={`${iconPath}eye.jpg`} alt=""/></div>
+              <div><img src={`${iconPath}ear.jpg`} onClick={this.openDebba} alt=""/></div>
+              <div><img src={`${iconPath}braille.jpg`} alt=""/></div>
+            </div>
+          }
+        {this.state.isOpen && <div id="mobileBox">
           <h1>Debba</h1>
           <ul className="header">
             <li><NavLink to="/">Home</NavLink></li>
@@ -27,12 +56,14 @@ class Main extends Component {
           </div>
           <div className="content">
           <iframe
+            title="debba"
             allow="microphone;"
             width="350"
             height="430"
             src="https://console.dialogflow.com/api-client/demo/embedded/0c993818-bd0c-4819-b5d3-fc4aadbb653b">
           </iframe>
           </div>
+        </div>}
         </div>
       </HashRouter>
     );
