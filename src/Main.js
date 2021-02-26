9848bff329de0f3233344398de895585e9ffa24c
pandyaanil <anil.pandya84@gmail.com>
2021-02-25 19:46:33 -0500

    style changes

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 // import ReactDOM from "react-dom";
 import Home from "./Home";
-import Stuff from "./Stuff";
 import Contact from "./Contact";
 
 const iconPath = process.env.PUBLIC_URL + '/assets/icons/';
@@ -33,7 +32,7 @@ class Main extends Component {
     return (
       <HashRouter>
         <div className="container">
-          <div className="avatar"><div onClick={this.openNavbar}><img src={`${iconPath}faceid.jpg`} alt=""/></div></div>
+          <div className="avatar"><div onClick={this.openNavbar}><img src={`${iconPath}AssebilityIcon.jpg`} alt=""/></div></div>
           {this.state.isNavbarOpen && 
             <div className="navbar">
               <div><img src={`${iconPath}screenreader.jpg`} alt=""/></div>
@@ -44,21 +43,11 @@ class Main extends Component {
           }
         {this.state.isOpen && <div id="mobileBox">
           <h1>Debba</h1>
-          <ul className="header">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/contact">Payments</NavLink></li>
-            <li><NavLink to="/stuff">:</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
-            <Route path="/contact" component={Contact}/>
-          </div>
           <div className="content">
           <iframe title="debbaChatbot"
             allow="microphone;"
-            width="350"
-            height="430"
+            width="405"
+            height="550"
             src="https://console.dialogflow.com/api-client/demo/embedded/0c993818-bd0c-4819-b5d3-fc4aadbb653b">
           </iframe>
           </div>
